9d4a008e29891fd11f2457b78c5abf42489ad318
I-Shen <isliao613@gmail.com>
2026-01-20 14:15:08 +0800

    chore: remove unused files and rename diagram (#41)
    - Remove unused duckdb, oracle configs, build scripts
    - Remove legacy tool install scripts (replaced by .tools target)
    - Remove orphaned compare-schemas.sh script
    - Rename dbrs diagram to kafka-dbsync

--- tools/autocompletion/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=$(dirname "$0")
-
-sudo apt-get install bash-completion
-echo 'source <(kubectl completion bash)' >>~/.bashrc
-echo 'source /usr/share/bash-completion/bash_completion' >> ~/.bashrc
-source ~/.bashrc
--- tools/kubectl/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -sLO https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl \
-    && chmod +x ./kubectl \
-    && sudo mv ./kubectl /usr/local/bin/
